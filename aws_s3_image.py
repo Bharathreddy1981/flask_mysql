e1599b03891dd402a94a0de18b2883671478367f
Bharathreddy1981 <70434245+Bharathreddy1981@users.noreply.github.com>
2020-10-19 17:54:14 +0530

    aws_image_s3
    how to insert the image in the aws using s3

---
+++ aws_s3_image.py
@@ -0,0 +1,18 @@
+import boto3
+from botocore.client import Config
+
+ACCESS_KEY_ID = "AKIAZNN5NBT72JCM2E6M"
+ACCESS_SECRET_KEY = "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"
+BUCKET_NAME = "flask121"
+
+data = open("baba.jpg", 'rb')
+
+s3 = boto3.resource(
+    "s3",
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=ACCESS_SECRET_KEY,
+    config=Config(signature_version="s3v4")
+)
+s3.Bucket(BUCKET_NAME).put_object(Key="bab.jpg", Body=data)
+
+print("Done")
