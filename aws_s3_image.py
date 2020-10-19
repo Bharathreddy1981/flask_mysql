57fa2283d1490b464750a5ab7fcc98f8e410cf9e
Bharathreddy1981 <70434245+Bharathreddy1981@users.noreply.github.com>
2020-10-19 18:11:34 +0530

    changing
    changing the accesskey and secretkey

--- aws_s3_image.py
+++ aws_s3_image.py
@@ -1,8 +1,8 @@
 import boto3
 from botocore.client import Config
 
-ACCESS_KEY_ID = "AKIAZNN5NBT72JCM2E6M"
-ACCESS_SECRET_KEY = "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"
+ACCESS_KEY_ID = "AKIAZNN5NBT7VXAFB64K"
+ACCESS_SECRET_KEY = "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"
 BUCKET_NAME = "flask121"
 
 data = open("baba.jpg", 'rb')
@@ -16,3 +16,4 @@ s3 = boto3.resource(
 s3.Bucket(BUCKET_NAME).put_object(Key="baba.jpg", Body=data)
 
 print("Done")
+
